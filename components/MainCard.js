281d4365541f7de5eb970687cfb55c7d20f2ff45
Jeffery Ho <chiachun2491@gmail.com>
2022-09-12 19:27:27 +0800

    feat: more flexible layout

--- components/MainCard.js
+++ components/MainCard.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function MainCard({ children, leftContent }) {
   return (
-    <section className="w-full md:w-1/2 lg:w-7/12 shadow-2xl flex flex-col md:flex-row rounded-lg flex-shrink">
+    <section className="w-full lg:max-w-3xl lg:w-3/4 xl:w-8/12 shadow-2xl flex flex-col md:flex-row rounded-lg flex-shrink place-self-center">
       <div className="w-full md:w-5/12 bg-sky-800 text-center min-h-full rounded-t-lg md:rounded-l-lg md:rounded-tr-none flex flex-col p-6">
         <div className="p-6 flex items-center justify-center flex-grow">
           <div className="p-6 bg-white rounded-full aspect-square leading-[0] shadow-lg">
